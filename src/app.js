301130f757c6985b49d076efc04423fbd0d0b36c
Alex <alex.lenoxparkinc@gmail.com>
2020-12-09 17:03:54 +0100

    Updates

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import { DateTime as LuxonDateTime } from 'luxon'
 import Datetime from '../../dist/vue-datetime'
-import '../../dist/vue-datetime.css'
+// import '../../dist/vue-datetime.css'
 import './app.css'
 
 Vue.use(Datetime)
